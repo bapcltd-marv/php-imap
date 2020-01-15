3a5964c77763cd1ea0ee06c63b40be1adce0490a
Marv <marv@bapc.ltd>
2020-01-15 15:59:39 +0000

    removing todos restored in rebase

--- src/PhpImap/Mailbox.php
+++ src/PhpImap/Mailbox.php
@@ -689,8 +689,6 @@ class Mailbox
 
     /**
      * Add the flag \Seen to a mail.
-     *
-     * @todo query appropriate param type
      */
     public function markMailAsRead(string $mailId): void
     {
@@ -699,8 +697,6 @@ class Mailbox
 
     /**
      * Remove the flag \Seen from a mail.
-     *
-     * @todo query appropriate param type
      */
     public function markMailAsUnread(string $mailId): void
     {
@@ -709,8 +705,6 @@ class Mailbox
 
     /**
      * Add the flag \Flagged to a mail.
-     *
-     * @todo query appropriate param type
      */
     public function markMailAsImportant(string $mailId): void
     {
